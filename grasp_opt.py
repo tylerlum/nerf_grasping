89bfa46c0a7c973a4cd13a781c2daacc9bf24592
krishnan <krshna@stanford.edu>
2022-04-11 13:07:03 -0700

    added grasping metrics to grasp_opt.py

--- grasp_opt.py
+++ grasp_opt.py
@@ -3,6 +3,15 @@ Module implementing methods for grasp optimization with NeRFs.
 """
 import grasp_utils
 import torch
+import numpy as np
+import cvxopt as cvx
+import sys
+import logging
+
+from pyhull import convex_hull as cvh
+from functools import partial
+
+# from cvxpylayers.torch import CvxpyLayer
 
 
 def grasp_matrix(grasp_points, normals):
@@ -92,11 +101,108 @@ def msv(grasp_points, normals):
 
 
 def ferrari_canny(grasp_points, normals):
+    """Calculates Ferrari Canny L1 grasp metric"""
     G = grasp_matrix(grasp_points, normals)
+    if len(G.size()) == 3:
+        dists = []
+        for g in G:
+            dists.append(fc(g.cpu().numpy()))
+        dist = torch.as_tensor(dists).float()
+    else:
+        dist = fc(G)
+    return dist
+
+
+def fc(G):
+    hull = cvh.ConvexHull(G.T)
+    min_norm_in_hull, v = min_norm_vector_in_facet(G)
+    if len(hull.vertices) == 0:
+        logging.debug("Convex hull could not be computed")
+        return 0.0
+
+    if min_norm_in_hull > 1e-3:
+        logging.debug("Min norm in hull not negative")
+        return 0.0
+
+    if np.sum(v > 1e-4) <= G.shape[0] - 1:
+        logging.debug("Zero not in interior of convex hull")
+        return 0.0
+
+    min_dist = sys.float_info.max
+    closest_facet = None
+    for v in hull.vertices:
+        if (
+            np.max(np.array(v)) < G.shape[1]
+        ):  # because of some occasional odd behavior from pyhull
+            facet = G[:, v]
+            dist, _ = min_norm_vector_in_facet(facet)
+            if dist < min_dist:
+                min_dist = dist
+                closest_facet = v
+    del closest_facet
+    return min_dist
+
+
+def min_norm_vector_in_facet(facet, wrench_regularizer=1e-10):
+    """Finds the minimum norm point in the convex hull of a given facet (aka simplex) by solving a QP.
+    Parameters
+    ----------
+    facet : 6xN :obj:`numpy.ndarray`
+        vectors forming the facet
+    wrench_regularizer : float
+        small float to make quadratic program positive semidefinite
+    Returns
+    -------
+    float
+        minimum norm of any point in the convex hull of the facet
+    Nx1 :obj:`numpy.ndarray`
+        vector of coefficients that achieves the minimum
+    """
+    dim = facet.shape[1]  # num vertices in facet
+
+    # create alpha weights for vertices of facet
+    G = facet.T @ facet
+    grasp_matrix = G + wrench_regularizer * np.eye(G.shape[0])
+
+    # Solve QP to minimize .5 x'Px + q'x subject to Gx <= h, Ax = b
+
+    P = cvx.matrix(2 * grasp_matrix)  # quadratic cost for Euclidean dist
+    q = cvx.matrix(np.zeros((dim, 1)))
+    G = cvx.matrix(-np.eye(dim))  # greater than zero constraint
+    h = cvx.matrix(np.zeros((dim, 1)))
+    A = cvx.matrix(np.ones((1, dim)))  # sum constraint to enforce convex
+    b = cvx.matrix(np.ones(1))  # combinations of vertices
+
+    cvx.solvers.options["show_progress"] = False
+    sol = cvx.solvers.qp(P, q, G, h, A, b)
+    v = np.array(sol["x"])
+    min_norm = np.sqrt(sol["primal objective"])
+
+    return abs(min_norm), v
+
+
+def l1_metric(grasp_points_t, normals_t, centroid=None, mu=1.0, num_edges=10):
+    """L1 Grasp quality metric using PyFastGrasp. Assumes object center of mass is at origin"""
+    import fastgrasp as fg
+
+    grasp_points = grasp_points_t.cpu().numpy().reshape(-1, 9)
+    normals = normals_t.cpu().numpy().reshape(-1, 9)
+    centroid = np.zeros((len(grasp_points), 3))
+    grasps = np.concatenate([grasp_points, normals, centroid], axis=1)
+    result = np.zeros(len(grasps))
+    _ = fg.getLowerBoundsPurgeQHull(grasps, mu, num_edges, result)
+    return torch.tensor(result)
 
 
 def optimize_cem(
-    cost, mu_0, Sigma_0, num_iters=25, num_samples=250, elite_frac=0.1, constraint=None
+    cost,
+    mu_0,
+    Sigma_0,
+    num_iters=25,
+    num_samples=250,
+    elite_frac=0.1,
+    constraint=None,
+    centroid=np.zeros((3, 1)),
 ):
     """
     Implements the cross-entropy method to optimize a given cost function.
@@ -114,7 +220,7 @@ def optimize_cem(
     for ii in range(num_iters):
         # Sample points from current distribution.
         if constraint:
-            x = rejection_sample(mu, Sigma, constraint, num_samples)
+            x = grasp_utils.rejection_sample(mu, Sigma, constraint, num_samples)
         else:
             x = (
                 mu.reshape(1, n, 1)
@@ -171,6 +277,7 @@ def grasp_cost(
     num_grasps=10,
     chunk=1024 * 32,
     cost_fn="psv",
+    l1_kwargs=dict(centroid=np.zeros((3, 1))),
 ):
 
     gps = grasp_vars.reshape(-1, n_f, 6)
@@ -179,7 +286,7 @@ def grasp_cost(
     grasp_points, grad_ests, grasp_mask = grasp_utils.sample_grasps(
         gps, num_grasps, coarse_model, fine_model, renderer
     )
-    # Reshape grasp points and grads for msv evaluation.
+    # Reshtorch grasp points and grads for msv evaluation.
     grasp_points = grasp_points.reshape(-1, n_f, 3)
     grad_ests = grad_ests.reshape(-1, n_f, 3)
 
@@ -187,6 +294,10 @@ def grasp_cost(
         cost_fn = psv
     elif cost_fn == "msv":
         cost_fn = msv
+    elif cost_fn == "fc":
+        cost_fn = ferrari_canny
+    elif cost_fn == "l1":
+        cost_fn = partial(l1_metric, **l1_kwargs)
     g_cost = torch.mean(cost_fn(grasp_points, grad_ests).reshape(B, num_grasps), dim=-1)
 
     g_cost = torch.where(
@@ -197,19 +308,33 @@ def grasp_cost(
 
 
 def get_points_cem(
-    nerf, n_f, mu_scale=5e-3, sigma_scale=1e-2, mu_0=None, Sigma_0=None, constraint=None
+    n_f,
+    coarse_model,
+    fine_model,
+    renderer,
+    mu_scale=5e-3,
+    sigma_scale=1e-3,
+    mu_0=None,
+    Sigma_0=None,
+    num_samples=750,
+    num_iters=10,
+    constraint=None,
+    cost_fn="msv",
 ):
 
+    # grasp vars are 2 * 3 * number of fingers, since include both pos and direction
     if not mu_0:
-        mu_0 = mu_scale * torch.randn(3 * n_f)
+        mu_0 = mu_scale * torch.randn(6 * n_f)
 
     if not Sigma_0:
-        Sigma_0 = sigma_scale * torch.eye(3 * n_f)
+        Sigma_0 = sigma_scale * torch.eye(6 * n_f)
 
-    cost = lambda x: grasp_cost(x, nerf, n_f)
-
-    mu_f, Sigma_f = optimize_cem(
-        cost, mu_0, Sigma_0, num_iters=25, num_samples=500, constraint=constraint
+    cost = lambda x: grasp_cost(
+        x, n_f, coarse_model, fine_model, renderer, cost_fn=cost_fn
     )
 
-    return mu_f.reshape(n_f, 3)
+    mu_f, Sigma_f = optimize_cem(
+        cost, mu_0, Sigma_0, num_iters=10, num_samples=500, constraint=constraint
+    )
+
+    return mu_f.reshape(n_f, 6)

ebacdcea272f3cdd7529236150889d02b227ecb9
Preston Culbertson <pculbertson@stanford.edu>
2022-06-11 00:09:18 -0700

    removing weird change to normal computation

--- nerf_grasping/grasp_utils.py
+++ nerf_grasping/grasp_utils.py
@@ -281,7 +281,7 @@ def sample_grasps(grasp_vars, num_grasps, model, residual_dirs=True, centroid=0.
     return grasp_points, grad_ests, grasp_mask
 
 def est_grads_vals(
-    nerf, grasp_points, method="central_difference", sigma=5e-4, num_samples=1000
+    nerf, grasp_points, method="central_difference", sigma=1e-3, num_samples=1000
 ):
     """
     Uses sampling to estimate gradients and density values for

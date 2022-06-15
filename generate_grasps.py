fixed some scaling/mass prop issues for box URDF

generate_grasps.py
@@ -40,40 +40,29 @@ def main(
     dice_grasp=False,
     cost_fn="l1",
 ):
-    if obj_name == "teddy_bear":
-        object_bounds = [(-0.1, 0.1), (0.01, 0.15), (-0.1, 0.1)]
-    elif obj_name == "banana":
-        object_bounds = [(-0.1, 0.1), (0.01, 0.05), (-0.1, 0.1)]
+
+    object_bounds = [(-0.1, 0.1), (0.01, 0.15), (-0.1, 0.1)]
+
+    if obj_name == "banana":
+        obj = ig_objects.Banana()
+    elif obj_name == "box":
+        obj = ig_objects.Box()
+    elif obj_name == "teddy_bear":
+        obj = ig_objects.TeddyBear()
+        obj.use_centroid = True
+    elif obj_name == "powerdrill":
+        obj = ig_objects.PowerDrill()
 
     if use_nerf:
-        if obj_name == "banana":
-            obj = ig_objects.Banana
-        elif obj_name == "box":
-            obj = ig_objects.Box
-        elif obj_name == "teddy_bear":
-            obj = ig_objects.TeddyBear
-            obj.use_centroid = True
-        elif obj_name == "powerdrill":
-            obj = ig_objects.PowerDrill
-
         model = ig_objects.load_nerf(obj.workspace, obj.bound, obj.scale)
         centroid = grasp_utils.get_centroid(model, object_bounds)
         print(f"Estimated Centroid: {centroid}")
         print(f"True Centroid: {get_mesh_centroid(obj_name)}")
 
     else:
-        if mesh_in is not None:
-            obj_mesh = mesh_in
-        elif obj_name == "banana":
-            obj_mesh = "assets/objects/meshes/banana/textured.obj"
-        elif obj_name == "box":
-            obj_mesh = "assets/objects/meshes/cube_multicolor.obj"
-        elif obj_name == "teddy_bear":
-            obj_mesh = "assets/objects/meshes/isaac_teddy/isaac_bear.obj"
-        elif obj_name == "powerdrill":
-            obj_mesh = "assets/objects/meshes/power_drill/textured.obj"
+        obj.load_trimesh()
+        gt_mesh = obj.gt_mesh
 
-        gt_mesh = trimesh.load(obj_mesh, force="mesh")
         T = np.eye(4)
         R = scipy.spatial.transform.Rotation.from_euler("Y", [-np.pi / 2]).as_matrix()
         R = (

grasp_test.py
@@ -169,7 +169,7 @@ def object_pos_control(
     in_normal,
     target_position=None,
     target_normal=2.5,
-    kp=0.1,
+    kp=10.,
     kd=0.1,
     kp_angle=0.04,
     kd_angle=0.001,
@@ -291,7 +291,7 @@ def lifting_trajectory(grasp_vars, mesh=None):
                     pos_offset=obj.position,
                     rot_offset=obj.orientation,
                 )
-                ge = -torch.tensor(ge, dtype=torch.float32)
+                ge = torch.tensor(ge, dtype=torch.float32)
             f_lift, target_force, target_torque = object_pos_control(
                 obj, ge, target_normal=0.4, kp=1.5, kd=1.0, kp_angle=0.1, kd_angle=1e-2
             )
@@ -335,7 +335,7 @@ env = setup_env()
 setup_stage(env)
 viewer = setup_viewer() if visualization else None
 
-Obj = ig_objects.TeddyBear
+Obj = ig_objects.Box
 grasp_points, grasp_normals = Obj.grasp_points, Obj.grasp_normals
 
 grasp_normals = grasp_normals / grasp_normals.norm(dim=1, keepdim=True)
@@ -346,6 +346,7 @@ robot = FingertipRobot(
     gym, sim, env, grasp_vars=grasp_vars, use_grad_est=True, norm_start_offset=0.1
 )
 obj = Obj(gym, sim, env)
+print(obj.mass)
 
 robot.setup_tensors()
 obj.setup_tensors()
@@ -354,7 +355,7 @@ obj.load_trimesh()
 for i in range(4):
     step_gym()
 
-grasp_data = "grasp_data/banana_diced.npy"
+grasp_data = "grasp_data/box.npy"
 nerf = "nerf" in grasp_data
 if not nerf:
     mesh_name = grasp_data.split("/")[1].rstrip(".npy")

nerf_grasping/sim/ig_objects.py
@@ -45,13 +45,15 @@ class RigidObject:
     use_centroid = False
     mu = 1.0
 
-    def __init__(self, gym, sim, env):
+    def __init__(self, gym=None, sim=None, env=None):
         self.gym = gym
         self.sim = sim
         self.env = env
 
-        self.asset = self.create_asset()
-        self.actor = self.configure_actor(gym, env)
+        if self.sim is not None:
+            self.asset = self.create_asset()
+            self.actor = self.configure_actor(gym, env)
+
         self.nerf_loaded = False
         self.gt_mesh = None
 
@@ -95,6 +97,7 @@ class RigidObject:
         # T_rot[:3, :3] = R
         # self.gt_mesh.apply_transform(T)
         # self.gt_mesh.apply_transform(T_rot)
+        self.gt_mesh.apply_scale(self.obj_scale)
 
     def create_asset(self):
         asset_options = gymapi.AssetOptions()
@@ -207,55 +210,57 @@ class Box(RigidObject):
     grasp_points = torch.tensor(
         [[0.0, 0.05, 0.05], [0.03, -0.05, 0.05], [-0.03, -0.05, 0.05]]
     )
+    obj_scale = 0.05
 
     grasp_normals = torch.tensor([[0.0, -1.0, 0.0], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]])
     mesh_file = "objects/meshes/cube_multicolor.obj"
+    asset_file = "objects/urdf/cube_multicolor.urdf"
 
-    def create_asset(self):
-        asset_options = gymapi.AssetOptions()
+#     def create_asset(self):
+#         asset_options = gymapi.AssetOptions()
 
-        asset_options.vhacd_enabled = True
-        asset_options.mesh_normal_mode = gymapi.COMPUTE_PER_VERTEX
-        asset_options.override_inertia = False
-        asset_options.override_com = False
-        asset_options.density = 100
+#         asset_options.vhacd_enabled = True
+#         asset_options.mesh_normal_mode = gymapi.COMPUTE_PER_VERTEX
+#         asset_options.override_inertia = False
+#         asset_options.override_com = False
+#         asset_options.density = 100
 
-        asset_options.vhacd_params.mode = 0
-        asset_options.vhacd_params.resolution = 300000
-        asset_options.vhacd_params.max_convex_hulls = 10
-        asset_options.vhacd_params.max_num_vertices_per_ch = 16
+#         asset_options.vhacd_params.mode = 0
+#         asset_options.vhacd_params.resolution = 300000
+#         asset_options.vhacd_params.max_convex_hulls = 10
+#         asset_options.vhacd_params.max_num_vertices_per_ch = 16
 
-        asset = self.gym.create_box(self.sim, 0.1, 0.1, 0.1, asset_options)
+#         asset = self.gym.create_box(self.sim, 0.1, 0.1, 0.1, asset_options)
 
-        rs_props = self.gym.get_asset_rigid_shape_properties(asset)
-        for p in rs_props:
-            p.friction = 1.0
-            p.torsion_friction = 1.0
-            p.restitution = 0.1
-        self.gym.set_asset_rigid_shape_properties(asset, rs_props)
+#         rs_props = self.gym.get_asset_rigid_shape_properties(asset)
+#         for p in rs_props:
+#             p.friction = 1.0
+#             p.torsion_friction = 1.0
+#             p.restitution = 0.1
+#         self.gym.set_asset_rigid_shape_properties(asset, rs_props)
 
-        return asset
+#         return asset
 
-    def configure_actor(self, gym, env):
-        actor = self.gym.create_actor(
-            env,
-            self.asset,
-            gymapi.Transform(p=gymapi.Vec3(0.0, 0.0, 0.101)),
-            "box",
-            0,
-            0,
-            segmentationId=2,
-        )
-        self.gym.set_rigid_body_color(
-            self.env, actor, 0, gymapi.MESH_VISUAL, gymapi.Vec3(0.8, 0.2, 0.3)
-        )
+#     def configure_actor(self, gym, env):
+#         actor = self.gym.create_actor(
+#             env,
+#             self.asset,
+#             gymapi.Transform(p=gymapi.Vec3(0.0, 0.0, 0.101)),
+#             "box",
+#             0,
+#             0,
+#             segmentationId=2,
+#         )
+#         self.gym.set_rigid_body_color(
+#             self.env, actor, 0, gymapi.MESH_VISUAL, gymapi.Vec3(0.8, 0.2, 0.3)
+#         )
 
-        rigid_body_props = self.gym.get_actor_rigid_body_properties(self.env, actor)
-        self.mass = sum(x.mass for x in rigid_body_props)
-        com = rigid_body_props[0].com
-        self.CG = torch.tensor([com.x, com.y, com.z])
+#         rigid_body_props = self.gym.get_actor_rigid_body_properties(self.env, actor)
+#         self.mass = sum(x.mass for x in rigid_body_props)
+#         com = rigid_body_props[0].com
+#         self.CG = torch.tensor([com.x, com.y, com.z])
 
-        return actor
+#         return actor
 
 
 class PowerDrill(RigidObject):
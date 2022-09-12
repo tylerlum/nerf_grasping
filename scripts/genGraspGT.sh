e25c1ed855f00b162ee835db61bf8a8a0b640a70
Krishnan Srinivasan <krshna@stanford.edu>
2022-09-12 13:42:02 +0000

    updated scripts to run eval nerf and gen gt grasps

--- scripts/genGraspGT.sh
+++ scripts/genGraspGT.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
-for OBJ in 'BOX' 'TEDDY_BEAR' 'POWER_DRILL' 'BLEACH_CLEANSER'
+for OBJ in 'BANANA' 'BOX' 'TEDDY_BEAR' 'POWER_DRILL' 'BLEACH_CLEANSER' 'BIG_BANANA'
 do
-    python generate_grasps.py --wandb --object ${OBJ} --num-grasps 20 model-config:mesh
+    python generate_grasps.py --object ${OBJ} --num-grasps 20 \
+        model-config:nerf --model-config.expected-surface --model-config.expected-gradient
     if [[ $? != 0 ]]
     then
         break

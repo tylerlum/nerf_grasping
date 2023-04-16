aac89359087ba1ef21ac9cf5dd8351d55ee6ce73
Tyler Lum <tylergwlum@gmail.com>
2023-04-16 16:06:16 -0700

    Create create_nerf_datasets_all_acronym_objs.py first try

---
+++ create_nerf_datasets_all_acronym_objs.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+from nerf_grasping.sim import acronym_objects
+from tqdm import tqdm
+
+"""
+Create nerf dataset for all objects in the acronym dataset
+
+For example:
+```
+python sim_trifinger.py --get_nerf_training_data --obj Obj_Fish_5e6656b0f124e0f38af30108ea9ccb6c_0_019030 --viewer --num_steps_before_collecting 50 --overwrite
+```
+"""
+
+acronym_object_classes = [k for k in acronym_objects.__dict__.keys() if k.startswith("Obj_")]
+print(f"Found {len(acronym_object_classes)} objects in the acronym dataset")
+print(f"First 10: {acronym_object_classes[:10]}")
+
+for acronym_object_class in tqdm(acronym_object_classes):
+    command = f"python sim_trifinger.py --get_nerf_training_data --obj {acronym_object_class} --viewer --num_steps_before_collecting 50 --overwrite"
+    print(f"Running command: {command}")
+    subprocess.run(command, shell=True, check=True)

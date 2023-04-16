a6e4a5fb35e24bb971c2a1dd03666f1a73b0fb83
Tyler Lum <tylergwlum@gmail.com>
2023-04-16 16:11:10 -0700

    Fix typo with viewer

--- create_nerf_datasets_all_acronym_objs.py
+++ create_nerf_datasets_all_acronym_objs.py
@@ -1,5 +1,4 @@
 import subprocess
-import os
 from nerf_grasping.sim import acronym_objects
 from tqdm import tqdm
 
@@ -8,7 +7,7 @@ Create nerf dataset for all objects in the acronym dataset
 
 For example:
 ```
-python sim_trifinger.py --get_nerf_training_data --obj Obj_Fish_5e6656b0f124e0f38af30108ea9ccb6c_0_019030 --viewer --num_steps_before_collecting 50 --overwrite
+python sim_trifinger.py --get_nerf_training_data --obj Obj_Fish_5e6656b0f124e0f38af30108ea9ccb6c_0_019030 --num_steps_before_collecting 50 --overwrite
 ```
 """
 
@@ -17,6 +16,6 @@ print(f"Found {len(acronym_object_classes)} objects in the acronym dataset")
 print(f"First 10: {acronym_object_classes[:10]}")
 
 for acronym_object_class in tqdm(acronym_object_classes):
-    command = f"python sim_trifinger.py --get_nerf_training_data --obj {acronym_object_class} --viewer --num_steps_before_collecting 50 --overwrite"
+    command = f"python sim_trifinger.py --get_nerf_training_data --obj {acronym_object_class} --num_steps_before_collecting 50 --overwrite"
     print(f"Running command: {command}")
     subprocess.run(command, shell=True, check=True)

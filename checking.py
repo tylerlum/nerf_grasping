c1da1b5689695518a6eaa9ef3f5a822491a85eda
Tyler Lum <tylergwlum@gmail.com>
2023-04-22 18:51:23 -0700

    Have checking.py work

--- checking.py
+++ checking.py
@@ -1,5 +1,5 @@
 from hydra import compose, initialize
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import DictConfig, OmegaConf, MISSING
 from dataclasses import dataclass, field
 
 from hydra.core.config_store import ConfigStore
@@ -8,32 +8,32 @@ OmegaConf.register_new_resolver("eval", eval, replace=True)
 # Define your data classes
 @dataclass
 class WandbConfig:
-    entity: str
-    project: str
-    name: str
-    group: str
-    job_type: str
+    entity: str = MISSING
+    project: str = MISSING
+    name: str = MISSING
+    group: str = MISSING
+    job_type: str = MISSING
 
 
 @dataclass
 class DataConfig:
-    frac_val: float
-    frac_test: float
-    frac_train: float
-    input_dataset_dir: str
+    frac_val: float = MISSING
+    frac_test: float = MISSING
+    frac_train: float = MISSING
+    input_dataset_dir: str = MISSING
 
 
 @dataclass
 class Config:
-    wandb: WandbConfig
-    data: DataConfig
+    wandb: WandbConfig = MISSING
+    data: DataConfig = MISSING
 
 # Add your config to the ConfigStore
 config_store = ConfigStore.instance()
 config_store.store(name="config", node=Config)
 
 # Initialize Hydra
-with initialize(config_path="./Train_NeRF_Grasp_Metric_cfg"):
+with initialize(version_base="1.2", config_path="./Train_NeRF_Grasp_Metric_cfg"):
     # Load your config
     cfg: DictConfig = compose(config_name="config")
     # Convert it to a structured config using OmegaConf

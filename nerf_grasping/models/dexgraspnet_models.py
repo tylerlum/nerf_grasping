5ac0a06b97b358c7d3414bf20205f88b4ceb3517
Tyler Lum <tylergwlum@gmail.com>
2023-08-20 21:13:02 -0700

    Reduce model size and change to flatten

--- nerf_grasping/models/dexgraspnet_models.py
+++ nerf_grasping/models/dexgraspnet_models.py
@@ -13,10 +13,10 @@ class CNN_3D_Classifier(nn.Module):
 
         self.conv = conv_encoder(
             input_shape=self.input_shape,
-            conv_channels=[64, 128, 256],
+            conv_channels=[32, 64, 128],
             pool_type=PoolType.MAX,
             dropout_prob=0.1,
-            conv_output_to_1d=ConvOutputTo1D.AVG_POOL_SPATIAL,
+            conv_output_to_1d=ConvOutputTo1D.FLATTEN,
         )
 
         # Get conv output shape
@@ -37,7 +37,7 @@ class CNN_3D_Classifier(nn.Module):
         self.mlp = mlp(
             num_inputs=self.conv_output_dim * self.n_fingers,
             num_outputs=self.n_classes,
-            hidden_layers=[512, 512],
+            hidden_layers=[256, 256],
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:

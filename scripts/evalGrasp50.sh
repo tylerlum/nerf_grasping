495b169b7cebd3bac52b0515b8b2667a41f5a8bb
Preston Culbertson <pculbert@caltech.edu>
2023-08-26 10:48:48 -0700

    Big cleanup stripping out old sim / nerf code

--- scripts/evalGrasp50.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-for OBJ in 'BANANA' 'BOX' 'TEDDY_BEAR' 'POWER_DRILL' 'BLEACH_CLEANSER'
-do
-    python grasp_test.py --wandb --object ${OBJ} model-config:mesh --model-config.level-set 50
-    if [[ $? != 0 ]]
-    then
-        break
-    fi
-done
--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="nerf_grasping",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
 )

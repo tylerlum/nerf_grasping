0322c286d3f06a200909c3204aae482c451b3362
Preston Culbertson <pculbert@caltech.edu>
2023-09-11 23:04:14 -0700

    add support for multiprocessing to nerf training

--- nerf_grasping/dataset/Train_All_Dataset_NeRFs.py
+++ nerf_grasping/dataset/Train_All_Dataset_NeRFs.py
@@ -3,6 +3,7 @@ import pathlib
 import tyro
 from typing import Optional
 import os
+import multiprocessing
 
 
 @dataclass
@@ -14,6 +15,30 @@ class NerfTrainingConfig:
     model: str = "nerfacto"
     num_iters: int = 200
     scene_scale: float = 0.2
+    use_multiprocess: bool = True
+    num_workers: int = 8
+
+
+def run_command(object_code_and_scale_str, cfg, nerfcheckpoints_path):
+    nerf_training_command = (
+        "ns-train nerfacto"
+        + f" --data {cfg.nerfdata_path / object_code_and_scale_str}"
+        + f" --max-num-iterations {cfg.num_iters}"
+        + f" --output-dir {nerfcheckpoints_path}"
+        + f" --vis wandb"
+        + f" --viewer.quit-on-train-completion True"
+        + f" --pipeline.model.disable-scene-contraction True"
+        + f" --pipeline.model.background-color black"
+        + f" nerfstudio-data"
+        + f" --auto-scale-poses False"
+        + f" --scale-factor 1."
+        + f" --scene-scale {cfg.scene_scale}"
+        + f" --center-method none"
+        + f" --orientation-method none"
+    )
+
+    print(f"Running: {nerf_training_command}")
+    os.system(nerf_training_command)
 
 
 def main(cfg: NerfTrainingConfig):
@@ -25,25 +50,18 @@ def main(cfg: NerfTrainingConfig):
     print(f"Found {len(object_code_and_scale_strs)} objects.")
     print(f"Saving checkpoints to {nerfcheckpoints_path}")
 
-    for object_code_and_scale_str in object_code_and_scale_strs:
-        nerf_training_command = (
-            "ns-train nerfacto"
-            + f" --data {cfg.nerfdata_path / object_code_and_scale_str}"
-            + f" --max-num-iterations {cfg.num_iters}"
-            + f" --output-dir {nerfcheckpoints_path}"
-            + f" --vis wandb"
-            + f" --pipeline.model.disable-scene-contraction True"
-            + f" --pipeline.model.background-color black"
-            + f" nerfstudio-data"
-            + f" --auto-scale-poses False"
-            + f" --scale-factor 1."
-            + f" --scene-scale {cfg.scene_scale}"
-            + f" --center-method none"
-            + f" --orientation-method none"
-        )
-
-        print(f"Running: {nerf_training_command}")
-        os.system(nerf_training_command)
+    if cfg.use_multiprocess:
+        with multiprocessing.Pool(cfg.num_workers) as pool:
+            pool.starmap(
+                run_command,
+                [
+                    (object_code_and_scale_str, cfg, nerfcheckpoints_path)
+                    for object_code_and_scale_str in object_code_and_scale_strs
+                ],
+            )
+    else:
+        for object_code_and_scale_str in object_code_and_scale_strs:
+            run_command(object_code_and_scale_str, cfg, nerfcheckpoints_path)
 
 
 if __name__ == "__main__":

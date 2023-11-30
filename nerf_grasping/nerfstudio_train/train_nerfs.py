e27aecfeeca4661f0bfdeaecb37377bb23a7e91e
Tyler Lum <tylergwlum@gmail.com>
2023-11-30 02:25:28 -0800

    Fix typo

--- nerf_grasping/nerfstudio_train/train_nerfs.py
+++ nerf_grasping/nerfstudio_train/train_nerfs.py
@@ -40,7 +40,9 @@ def main() -> None:
     output_nerfcheckpoints_path = experiment_path / args.output_nerfcheckpoints_name
     output_nerfcheckpoints_path.mkdir(exist_ok=True)
 
-    for object_and_scale_nerfdata_path in tqdm(nerfdata_path.iterdir(), dynamic_ncols=True, desc="Training NERF")):
+    for object_and_scale_nerfdata_path in tqdm(
+        nerfdata_path.iterdir(), dynamic_ncols=True, desc="Training NERF"
+    ):
         if not object_and_scale_nerfdata_path.is_dir():
             continue
 

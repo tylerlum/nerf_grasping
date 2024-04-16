261dec38aa90beea916dcce877856d022ad9279b
Tyler Lum <tylergwlum@gmail.com>
2024-04-16 15:59:00 -0700

    Clean up

--- nerf_grasping/nerfstudio_train/train_nerfs_return_trainer.py
+++ nerf_grasping/nerfstudio_train/train_nerfs_return_trainer.py
@@ -2,20 +2,7 @@ from dataclasses import dataclass
 import pathlib
 
 from nerfstudio.scripts.train import _set_random_seed
-from nerfstudio.engine.trainer import TrainerConfig
-
-from nerfstudio.cameras.camera_optimizers import CameraOptimizerConfig
-from nerfstudio.configs.base_config import ViewerConfig
-
-from nerfstudio.data.datamanagers.base_datamanager import VanillaDataManagerConfig
-from nerfstudio.data.dataparsers.nerfstudio_dataparser import NerfstudioDataParserConfig
-from nerfstudio.engine.optimizers import AdamOptimizerConfig
-from nerfstudio.engine.schedulers import (
-    ExponentialDecaySchedulerConfig,
-)
 from nerfstudio.engine.trainer import TrainerConfig, Trainer
-from nerfstudio.models.nerfacto import NerfactoModelConfig
-from nerfstudio.pipelines.base_pipeline import VanillaPipelineConfig
 
 
 @dataclass
@@ -47,44 +34,9 @@ def train_loop_return_trainer(
 
 def get_nerfacto_default_config():
     # From nerfstudio/configs/method_configs.py nerfacto
-    return TrainerConfig(
-        method_name="nerfacto",
-        steps_per_eval_batch=500,
-        steps_per_save=2000,
-        max_num_iterations=30000,
-        mixed_precision=True,
-        pipeline=VanillaPipelineConfig(
-            datamanager=VanillaDataManagerConfig(
-                dataparser=NerfstudioDataParserConfig(),
-                train_num_rays_per_batch=4096,
-                eval_num_rays_per_batch=4096,
-                camera_optimizer=CameraOptimizerConfig(
-                    mode="SO3xR3",
-                    optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2),
-                    scheduler=ExponentialDecaySchedulerConfig(
-                        lr_final=6e-6, max_steps=200000
-                    ),
-                ),
-            ),
-            model=NerfactoModelConfig(eval_num_rays_per_chunk=1 << 15),
-        ),
-        optimizers={
-            "proposal_networks": {
-                "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
-                "scheduler": ExponentialDecaySchedulerConfig(
-                    lr_final=0.0001, max_steps=200000
-                ),
-            },
-            "fields": {
-                "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
-                "scheduler": ExponentialDecaySchedulerConfig(
-                    lr_final=0.0001, max_steps=200000
-                ),
-            },
-        },
-        viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
-        vis="viewer",
-    )
+    from nerfstudio.configs.method_configs import all_methods
+
+    return all_methods["nerfacto"]
 
 
 def train_nerf(
@@ -112,6 +64,10 @@ def train_nerf(
 
     # Need to set timestamp
     config.set_timestamp()
+
+    # print and save config
+    config.print_to_terminal()
+    config.save_config()
 
     trainer = train_loop_return_trainer(local_rank=0, world_size=1, config=config)
     return trainer

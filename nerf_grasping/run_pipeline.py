3926983a69fc7ba47e0795f1e8c97fdb073c803f
Tyler Lum <tylergwlum@gmail.com>
2024-05-06 16:23:04 -0700

    Add vis

--- nerf_grasping/run_pipeline.py
+++ nerf_grasping/run_pipeline.py
@@ -729,6 +729,27 @@ def run_curobo(
         qd_with_closing = np.concatenate([qd, interpolated_qds], axis=0)
         qds_with_closing.append(qd_with_closing)
 
+    ################ Visualize ################
+    from nerf_grasping.curobo_fr3_algr_zed2i.visualizer import (
+        start_visualizer,
+        draw_collision_spheres_default_config,
+        remove_collision_spheres_default_config,
+        set_robot_state,
+        animate_robot,
+        create_urdf,
+    )
+
+    OBJECT_URDF_PATH = create_urdf(obj_path=pathlib.Path("/tmp/mesh_viz_object.obj"))
+    pb_robot = start_visualizer(object_urdf_path=OBJECT_URDF_PATH)
+    draw_collision_spheres_default_config(pb_robot)
+    time.sleep(1.0)
+
+    remove_collision_spheres_default_config()
+    animate_robot(robot=pb_robot, qs=qs_with_closing[0], dt=dts[0])
+    ################ Visualize ################
+
+
+
     print("\n" + "=" * 80)
     print("Step 11: Add lifing motion")
     print("=" * 80 + "\n")
@@ -743,6 +764,16 @@ def run_curobo(
         q_fr3_starts=q_start_lifts[:, :7],
         q_algr_starts=q_start_lifts[:, 7:],
         X_W_Hs=X_W_H_lifts,
+        collision_check_object=False,
+        obj_filepath=pathlib.Path("/tmp/mesh_viz_object.obj"),
+        obj_xyz=(cfg.nerf_frame_offset_x, 0.0, 0.0),
+        obj_quat_wxyz=(1.0, 0.0, 0.0, 0.0),
+        collision_check_table=False,
+        use_cuda_graph=False,
+        enable_graph=True,
+        enable_opt=False,
+        timeout=10.0,
+        collision_sphere_buffer=0.01,
     )
     lift_motion_gen_success_idxs = (
         lift_motion_gen_result.success.flatten().nonzero().flatten().tolist()
@@ -777,6 +808,7 @@ def run_curobo(
             set(overall_success_idxs).intersection(set(lift_overall_success_idxs))
         )
     )
+    breakpoint()
 
     qs_with_lift, qds_with_lift = [], []
     for i, (q, qd, dt, lift_q, lift_qd, lift_dt) in enumerate(zip(qs_with_closing, qds_with_closing, dts, lift_qs, lift_qds, lift_dts)):
@@ -809,8 +841,136 @@ def run_curobo(
 
     return qs, qds, T_trajs, final_success_idxs
 
-def solve_lift_trajopt_ignore_hand_batch(q_fr3_starts: np.ndarray, q_algr_starts: np.ndarray, X_W_Hs: np.ndarray):
+import pathlib
+import time
+from typing import Optional, Tuple, List
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from curobo.cuda_robot_model.cuda_robot_model import (
+    CudaRobotModel,
+)
+
+from curobo.geom.sdf.world import CollisionCheckerType
+from curobo.wrap.reacher.motion_gen import (
+    MotionGen,
+    MotionGenConfig,
+    MotionGenPlanConfig,
+    MotionGenResult,
+)
+from curobo.types.base import TensorDeviceType
+from curobo.types.math import Pose
+from curobo.types.robot import RobotConfig, JointState
+from curobo.util_file import (
+    get_robot_configs_path,
+    join_path,
+    load_yaml,
+)
+from curobo.wrap.reacher.ik_solver import IKSolver, IKSolverConfig, IKResult
+
+from nerf_grasping.curobo_fr3_algr_zed2i.fr3_algr_zed2i_world import (
+    get_world_cfg,
+)
+from nerf_grasping.curobo_fr3_algr_zed2i.trajopt_fr3_algr_zed2i import (
+    DEFAULT_Q_FR3,
+    DEFAULT_Q_ALGR,
+)
+
+from nerf_grasping.optimizer_utils import (
+    is_in_limits,
+)
+
+
+def _sqrt_positive_part(x: torch.Tensor) -> torch.Tensor:
+    """
+    Returns torch.sqrt(torch.max(0, x))
+    subgradient is zero where x is 0.
+    """
+    ret = torch.zeros_like(x)
+    positive_mask = x > 0
+    ret[positive_mask] = torch.sqrt(x[positive_mask])
+    return ret
+
+
+def matrix_to_quat_wxyz(matrix: torch.Tensor) -> torch.Tensor:
+    """
+    Convert rotations given as rotation matrices to quat_wxyz.
+    Args:
+        matrix: Rotation matrices as tensor of shape (..., 3, 3).
+    Returns:
+        quat_wxyz with real part first, as tensor of shape (..., 4).
+    """
+    if matrix.size(-1) != 3 or matrix.size(-2) != 3:
+        raise ValueError(f"Invalid rotation matrix shape {matrix.shape}.")
+
+    batch_dim = matrix.shape[:-2]
+    m00, m01, m02, m10, m11, m12, m20, m21, m22 = torch.unbind(
+        matrix.reshape(batch_dim + (9,)), dim=-1
+    )
+
+    q_abs = _sqrt_positive_part(
+        torch.stack(
+            [
+                1.0 + m00 + m11 + m22,
+                1.0 + m00 - m11 - m22,
+                1.0 - m00 + m11 - m22,
+                1.0 - m00 - m11 + m22,
+            ],
+            dim=-1,
+        )
+    )
+
+    quat_by_rijk = torch.stack(
+        [
+            torch.stack([q_abs[..., 0] ** 2, m21 - m12, m02 - m20, m10 - m01], dim=-1),
+            torch.stack([m21 - m12, q_abs[..., 1] ** 2, m10 + m01, m02 + m20], dim=-1),
+            torch.stack([m02 - m20, m10 + m01, q_abs[..., 2] ** 2, m12 + m21], dim=-1),
+            torch.stack([m10 - m01, m20 + m02, m21 + m12, q_abs[..., 3] ** 2], dim=-1),
+        ],
+        dim=-2,
+    )
+
+    flr = torch.tensor(0.1).to(dtype=q_abs.dtype, device=q_abs.device)
+    quat_candidates = quat_by_rijk / (2.0 * q_abs[..., None].max(flr))
+
+    return quat_candidates[
+        F.one_hot(q_abs.argmax(dim=-1), num_classes=4) > 0.5, :
+    ].reshape(batch_dim + (4,))
+
+
+
+def solve_lift_trajopt_ignore_hand_batch(q_fr3_starts: np.ndarray, q_algr_starts: np.ndarray, X_W_Hs: np.ndarray,
+    collision_check_object: bool = True,
+    obj_filepath: Optional[pathlib.Path] = pathlib.Path(
+        "/juno/u/tylerlum/github_repos/nerf_grasping/experiments/2024-05-02_16-19-22/nerf_to_mesh/mug_330/coacd/decomposed.obj"
+    ),
+    obj_xyz: Tuple[float, float, float] = (0.65, 0.0, 0.0),
+    obj_quat_wxyz: Tuple[float, float, float, float] = (1.0, 0.0, 0.0, 0.0),
+    collision_check_table: bool = True,
+    use_cuda_graph: bool = False,  # Getting some errors from setting this to True
+    enable_graph: bool = True,
+    enable_opt: bool = False,  # Getting some errors from setting this to True
+    timeout: float = 5.0,
+    collision_sphere_buffer: Optional[float] = None,
+):
     N_GRASPS = X_W_Hs.shape[0]
+
+    tensor_args = TensorDeviceType()
+    robot_file = "fr3_algr_zed2i_with_fingertips.yml"
+    robot_cfg = load_yaml(join_path(get_robot_configs_path(), robot_file))["robot_cfg"]
+    if collision_sphere_buffer is not None:
+        robot_cfg["kinematics"]["collision_sphere_buffer"] = collision_sphere_buffer
+    robot_cfg = RobotConfig.from_dict(robot_cfg)
+
+    world_cfg = get_world_cfg(
+        collision_check_object=collision_check_object,
+        obj_filepath=obj_filepath,
+        obj_xyz=obj_xyz,
+        obj_quat_wxyz=obj_quat_wxyz,
+        collision_check_table=collision_check_table,
+    )
 
     trans = X_W_Hs[:, :3, 3]
     rot_matrix = X_W_Hs[:, :3, :3]
@@ -819,7 +979,6 @@ def solve_lift_trajopt_ignore_hand_batch(q_fr3_starts: np.ndarray, q_algr_starts
         torch.from_numpy(trans).float().cuda(),
         quaternion=quat_wxyz,
     )
-
 
     print("\n" + "=" * 80)
     print("Doing IK as sanity check")
@@ -837,7 +996,7 @@ def solve_lift_trajopt_ignore_hand_batch(q_fr3_starts: np.ndarray, q_algr_starts
     )
     ik_solver2 = IKSolver(ik_config2)
     ik_result2 = ik_solver2.solve_batch(
-        goal_pose=target_pose, link_poses=state.link_pose
+        goal_pose=target_pose
     )
 
     print("\n" + "=" * 80)
@@ -1136,7 +1295,7 @@ def main() -> None:
         )
 
     args.nerf_config = nerf_config
-    compute_grasps(nerf_model=nerf_model, cfg=args)
+    run_pipeline(nerf_model=nerf_model, cfg=args)
 
 
 if __name__ == "__main__":

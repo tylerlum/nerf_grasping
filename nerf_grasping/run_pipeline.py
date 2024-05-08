4e2658dbb0330d48aede7d2b3822586e608f8576
Tyler Lum <tylergwlum@gmail.com>
2024-05-08 05:01:39 -0700

    Clean up

--- nerf_grasping/run_pipeline.py
+++ nerf_grasping/run_pipeline.py
@@ -540,14 +540,9 @@ def run_curobo(
         collision_check_table=True,
         obj_name="mesh_object",
     )
-    # ik_solver.world_coll_checker.clear_cache()
     ik_solver.update_world(object_world_cfg)
-    # ik_solver2.world_coll_checker.clear_cache()
     ik_solver2.update_world(object_world_cfg)
-    # motion_gen.world_coll_checker.clear_cache()
-    breakpoint()
     motion_gen.update_world(object_world_cfg)
-    breakpoint()
     motion_gen_result, ik_result, ik_result2 = (
         # solve_trajopt_batch(
         #     X_W_Hs=X_W_Hs,
@@ -693,6 +688,20 @@ def run_curobo(
         X_W_H_lifts[i] = X_W_H_lifts[valid_idx]
 
     # Update world to remove object collision check
+    no_object_world_cfg = get_world_cfg(
+        collision_check_object=True,
+        obj_filepath=pathlib.Path(
+            # "/juno/u/tylerlum/github_repos/DexGraspNet/data/rotated_meshdata/core-bottle-1071fa4cddb2da2fc8724d5673a063a6/coacd/decomposed.obj"
+            "/tmp/mesh_viz_object.obj"
+        ),
+        obj_xyz=(cfg.nerf_frame_offset_x + HACK_OFFSET, 0.0, 1.0),
+        obj_quat_wxyz=(1.0, 0.0, 0.0, 0.0),
+        collision_check_table=True,
+        obj_name="no_object",
+    )
+    ik_solver.update_world(no_object_world_cfg)
+    ik_solver2.update_world(no_object_world_cfg)
+
     lift_motion_gen_result, lift_ik_result, lift_ik_result2 = (
         # solve_trajopt_batch(
         #     X_W_Hs=X_W_H_lifts,
@@ -1170,7 +1179,6 @@ def main() -> None:
             n_grasps=args.num_grasps,
             collision_check_object=True,
             obj_filepath=pathlib.Path("/juno/u/tylerlum/github_repos/DexGraspNet/data/rotated_meshdata/core-bottle-1071fa4cddb2da2fc8724d5673a063a6/coacd/decomposed.obj"),
-            # obj_filepath=pathlib.Path("/tmp/mesh_viz_object.obj"),
             obj_xyz=(args.nerf_frame_offset_x + HACK_OFFSET, 0.0, 0.0),
             obj_quat_wxyz=(1.0, 0.0, 0.0, 0.0),
             collision_check_table=True,

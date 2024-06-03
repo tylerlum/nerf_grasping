16224dac2b48bf670c312b90e178d90ae6b1279f
alberthli <alberthli@caltech.edu>
2024-06-03 10:57:35 -0700

    morning of jun 3 changes

--- nerf_grasping/dexdiffuser/train_dexdiffuser_evaluator.py
+++ nerf_grasping/dexdiffuser/train_dexdiffuser_evaluator.py
@@ -233,8 +233,7 @@ def _train_multigpu(rank, cfg):
 if __name__ == "__main__":
     cfg = DexEvaluatorTrainingConfig(
         num_epochs=1000,
-        # batch_size=4096 * 8,
-        batch_size=2048,
+        batch_size=4096 * 8,
         learning_rate=1e-4,
         device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
         random_seed=42,
